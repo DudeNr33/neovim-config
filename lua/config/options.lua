c3327aba088e2416b5336de550c6f57a8f933b4a
DudeNr33 <3929834+DudeNr33@users.noreply.github.com>
2025-04-20 20:24:25 +0200

    reduce timeout for terminal sequences

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.python3_host_prog = "~/.config/nvim/.venv/bin/python3"
+vim.o.timeoutlen = 300 -- timeout for 'normal' mappings
+vim.o.ttimeoutlen = 10 -- timeout for terminal sequences, e.g. involving ESC
 
 -- local powershell_options = {
 --   shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
