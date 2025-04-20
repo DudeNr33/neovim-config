cbad1cf3c75738deafacda806b462cdf901d4e26
DudeNr33 <3929834+DudeNr33@users.noreply.github.com>
2025-04-20 13:21:38 +0200

    set python provider, comment out powershell stuff

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,16 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.g.python3_host_prog = "~/.config/nvim/.venv/bin/python3"
 
-local powershell_options = {
-  shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
-  shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-  shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-  shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-  shellquote = "",
-  shellxquote = "",
-}
+-- local powershell_options = {
+--   shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+--   shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+--   shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+--   shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+--   shellquote = "",
+--   shellxquote = "",
+-- }
 
-for option, value in pairs(powershell_options) do
-  vim.opt[option] = value
-end
+-- for option, value in pairs(powershell_options) do
+--   vim.opt[option] = value
+-- end
